97aa16352320aeaf18f17738a3d1724851a20e0b
ussingdi <ussingdi3@gmail.com>
2024-11-17 17:52:07 -0600

    Refactoring

--- main.py
+++ main.py
@@ -8,40 +8,43 @@ with open('xgb_model.pkl', 'rb') as model_file:
 
 
 
-def preprocess_data(customer_data: dict):
+def preprocess_data(customer_dict: dict):
     input_dict = {
-        'CreditScore': customer_data['CreditScore'],
-        'Age': customer_data['Age'],
-        'Tenure': customer_data['Tenure'],
-        'Balance': customer_data['Balance'],
-        'NumOfProducts': customer_data['NumOfProducts'],
-        'HasCrCard': customer_data['HasCrCard'],
-        'IsActiveMember': customer_data['IsActiveMember'],
-        'EstimatedSalary': customer_data['EstimatedSalary'],
-        'Gender_Male': 1 if customer_data['Gender'] == 'Male' else 0,
-        'Gender_Female': 1 if customer_data['Gender'] == 'Female' else 0,
-        'Geography_France': 1 if customer_data['Geography'] == 'France' else 0,
-        'Geography_Germany': 1 if customer_data['Geography'] == 'Germany' else 0,
-        'Geography_Spain': 1 if customer_data['Geography'] == 'Spain' else 0,
+        'CreditScore': customer_dict['CreditScore'],
+        'Age': customer_dict['Age'],
+        'Tenure': customer_dict['Tenure'],
+        'Balance': customer_dict['Balance'],
+        'NumOfProducts': customer_dict['NumOfProducts'],
+        'HasCrCard': customer_dict['HasCrCard'],
+        'IsActiveMember': customer_dict['IsActiveMember'],
+        'EstimatedSalary': customer_dict['EstimatedSalary'],
+        'Gender_Female': 1 if customer_dict['Gender'] == 'Female' else 0,
+        'Geography_Germany': 1 if customer_dict['Geography'] == 'Germany' else 0,
+        'Geography_Spain': 1 if customer_dict['Geography'] == 'Spain' else 0,
+        'Gender_Male': 1 if customer_dict['Gender'] == 'Male' else 0,
+        'Geography_France': 1 if customer_dict['Geography'] == 'France' else 0,
     }
 
-    customer_data_df = pd.DataFrame([input_dict])
-    print(customer_data_df)
+    customer_df = pd.DataFrame([input_dict])
+    print(customer_df)
 
-    return customer_data_df
+    return customer_df
 
-def predict_churn(customer_data: dict):
-    processed_data = preprocess_data(customer_data)
-    prediction = loaded_model.predict(preprocess_data)
-    probability = loaded_model.predict_proba(preprocess_data)[:, 1]
+def get_predictions(customer_dict):
+    preprocessed_data = preprocess_data(customer_dict)
+    prediction = loaded_model.predict(preprocessed_data)
+    probability = loaded_model.predict_proba(preprocessed_data)[:, 1]
     return prediction, probability
 
 
 
 @app.post("/predict")
-async def predict(customer_data: dict):
-    prediction, probability = predict_churn(customer_data)
-    return {"prediction": prediction.tolist(), "probability": probability.tolist()}
+async def predict(data: dict):
+    prediction, probabilities = get_predictions(data)
+    return {
+        "prediction": prediction.tolist(), 
+        "probabilities": probabilities.tolist()
+        }
 
 if __name__ == "__main__":
     import uvicorn
